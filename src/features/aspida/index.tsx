683fca64b0c57208583edd4cde6910925a397dab
fffk10 <warafffk10@gmail.com>
2024-07-06 09:09:42 +0900

    aspidaお試し

--- src/features/aspida/index.tsx
+++ src/features/aspida/index.tsx
@@ -1,7 +1,38 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+import api from '../../../api/$api'
+import aspida from '@aspida/axios'
+import axios from 'axios'
+import { useState } from 'react'
+
+const client = api(
+  aspida(axios, { baseURL: 'https://668889880ea28ca88b8572c2.mockapi.io/' })
+)
+const queryClient = new QueryClient()
+
 export default function Aspida() {
+  const [users, setUsers] = useState<{ id: number; name: string }[]>([])
+
+  const handleGet = async () => {
+    const data = await client.v1.users.$get()
+    setUsers(data)
+  }
+
   return (
-    <div>
-      <h2>Aspida</h2>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h2>Aspida</h2>
+
+        <h3>GET /api/users</h3>
+        <button onClick={handleGet}>Get</button>
+
+        {users && (
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </QueryClientProvider>
   )
 }
